c41cd298b4907619e72387e7440cbf2ffa528d4e
Nathan Isom <nathanisom27@gmail.com>
2015-03-13 14:35:40 -0500

    Change to install packages after first boot, remove pacaur

--- config.sh
+++ config.sh
@@ -18,22 +18,26 @@ echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 locale-gen
 locale > /etc/locale.conf
 
-pacman -S sudo base-devel stow --noconfirm
+pacman -S sudo base-devel stow dialog wpa_supplicant --noconfirm
 echo "$user ALL=(ALL) ALL" >> /etc/sudoers
 
 mkdir -p /home/$user
 chown -R $user /home/$user
-
-pacman -Syyu --noconfirm
 
 export user=$user
 ./yaourt.sh
 
 chown $user dotfiles.sh
 su $user -c "./dotfiles.sh"
+
+# Install needed programs on first boot instead of while chroot'd
 chown $user install.sh packages_min.txt
-su $user -c "./install.sh packages_min.txt"
+cp install.sh /home/$user/install.sh
+cp packages_min.txt /home/$user/packages_min.txt
+
+# enable services here:
 
 
-#enable services here:
+# done, but still need bootloader so friendly reminder:
+echo "All set, up to you to setup boot :)"
 
